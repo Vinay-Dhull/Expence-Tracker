b6c5c67b12d196dc3f84c8cb4d2a41bbe897e62b
Vinay Dhull <137399808+Vinay-Dhull@users.noreply.github.com>
2024-07-17 19:42:00 +0530

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,28 +1,31 @@
+// Cron job to hit endpoint every 14 sec to keep backend alive always
 const cron = require('cron');
 const https = require('https');
 
-const backendUrl = 'provide_backend_api_endpoint_that_is_provided_by_rendor';
+const backendUrl = 'provide_backend_api_endpoint_that_is_provided_by_rendor' ;
+const job = new cron.CronJob('/14 * ** ', function () {
+// This function will be executed every 14 minutes.
+console. log('Restarting server');
 
-const job = new cron.CronJob('*/14 * * * * *', function() {
-  // This function will be executed every 14 seconds.
-  console.log('Pinging server to keep it alive');
+// Perform an HTTPS GET request to hit any backend api.
+https
+.get(backendUrl, (res) => {
+if (res.statusCode== 200) {
+console. log('Server restarted');
+} else {
+console. error(
+`failed to restart server with status code: ${res.statusCode}`
+   );
+  }
+})
+.on('error', (err) => {
+console.error('Error during Restart:', err.message);
 
-  // Perform an HTTPS GET request to hit the backend API.
-  https.get(backendUrl, (res) => {
-    if (res.statusCode === 200) {
-      console.log('Server responded successfully');
-    } else {
-      console.error(`Failed to reach server with status code: ${res.statusCode}`);
-    }
-  }).on('error', (err) => {
-    console.error('Error during request:', err.message);
-  });
+ });
+
 });
-
-// Start the cron job
-job.start();
 
 // Export the cron job.
 module.exports = {
-  job,
+job,
 };
