f2408941e7199704d40074a27878917e6d7eecb2
Vinay Dhull <137399808+Vinay-Dhull@users.noreply.github.com>
2024-07-17 10:46:03 +0530

    final code

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+// const path = require("path");
+const connectDb = require("./config/connectDb");
+ 
+dotenv.config();
+
+ connectDb();
+
+ const app = express();
+
+ app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+// Routes
+// User routes
+app.use("/api/v1/users", require("./routes/userRoute"));
+
+// Transaction routes (updated route name to match the frontend)
+app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
+
+// Serve static files from the React app
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+//  app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// Define the port
+const PORT = process.env.PORT || 8080;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
