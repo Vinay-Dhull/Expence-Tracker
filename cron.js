e2a7eca4fb32204a97528cbf8dd184247271702e
Vinay Dhull <137399808+Vinay-Dhull@users.noreply.github.com>
2024-07-17 20:08:48 +0530

    Update cron.js

--- cron.js
+++ cron.js
@@ -2,7 +2,7 @@ const cron = require('cron');
 const https = require('https');
 
 const backendUrl = 'https://expence-tracker-v5t2.onrender.com';
-const job = new cron.CronJob('*/14 * * * * *', function () {
+const job = new cron.CronJob("0 */14 * * * *", function () {
   // This function will be executed every 14 seconds.
   console.log('Pinging server to keep it alive');
 
